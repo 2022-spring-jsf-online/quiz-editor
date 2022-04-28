d819f71940199d475531874c7162550e7991ffc5
adiaz111 <adiaz8@madisoncollege.edu>
2022-04-28 15:29:36 -0500

    Newly added quiz badge working !!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ interface QuizDisplay {
   quizName: string;
   quizQuestions: QuestionDisplay[];
   markedForDelete: boolean;
+  newlyAddedQuiz: boolean;
 }
 
 interface QuestionDisplay {
@@ -30,6 +31,8 @@ export class AppComponent implements OnInit {
   loadQuizzesFromCloud = async () => {
 
     try {
+
+      this.loading = true;
       const quizzes = await this.quizSvc.loadQuizzes() ?? [];
       console.log(quizzes);
 
@@ -39,6 +42,7 @@ export class AppComponent implements OnInit {
           questionName: y.name
         }))
         , markedForDelete: false
+        , newlyAddedQuiz: false
       }));      
 
       this.loading = false;
@@ -69,6 +73,7 @@ export class AppComponent implements OnInit {
       quizName: "Untitled Quiz"
       , quizQuestions: []
       , markedForDelete: false
+      , newlyAddedQuiz: true
     };
 
     this.quizzes = [
@@ -163,6 +168,16 @@ export class AppComponent implements OnInit {
 
   get deletedQuizCount() {
     return this.getDeletedQuizzes().length;
-  }
+  };
+
+  getAddedQuizzes = () => {
+    return this.quizzes.filter(x => x.newlyAddedQuiz && !x.markedForDelete)
+  };
+
+  get addedQuizCount() {
+    return this.getAddedQuizzes().length;
+  };
+
+
 
 }
