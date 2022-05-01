3fa4abf29252b473bfb5d09921cdb2c2f7c091a9
ipoluboiarynov <ipoluboiarynov@madisoncollege.edu>
2022-05-01 12:23:23 -0500

    imported animation module and added animation to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
 import { FormsModule } from '@angular/forms';
-
 import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NgbModule,
     FormsModule,
     HttpClientModule
